88f9ea9555f31f0bdd5a879bf46dfc021c5a7208
OUMPHISITSAK <phisitsak.oum@gmail.com>
2023-04-26 21:56:56 +0700

    settion 12
    reactive form and validate

--- DTOs/UserDto.cs
+++ DTOs/UserDto.cs
@@ -6,5 +6,8 @@ namespace API.DTOs
     {
         public string Username { get; set; }
         public string token { get; set; }
+
+        public string PhotoUrl {get;set;}
+        public string KnownAs { get; set; }
     }
 }
